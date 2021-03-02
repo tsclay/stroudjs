9049106f8a7425d13a45d00d024bd7faea841069
Tim <tsclay9@gmail.com>
2021-03-02 16:41:19 -0500

    import and export all the things for the browser

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import {searchForOne, searchForAll, createElement, createSVG, nestElements, fragmentElements, removeNodes, empty } from'./dom'
+import {transition, pushSiblings, unshiftSiblings, flip } from'./animate'
+import * as easings from './easings'
+
+export { transition, pushSiblings, unshiftSiblings, flip, searchForOne, searchForAll, createElement, createSVG, nestElements, fragmentElements, removeNodes, empty, easings }
